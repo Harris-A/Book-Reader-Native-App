90fceb3e12d5aeca3ea871f97034bb2a7c12a255
Harris Alam <harrisalam3@gmail.com>
2025-06-24 09:59:02 +0100

    apply stack navigation styles

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,16 @@ import { Stack } from 'expo-router'
 
 const RootLayout = () => {
     return (
-        <Stack />
+        /* applies native stack navigator on app */
+        <Stack screenOptions={{
+            headerStyle: { backgroundColor: '#f4511e' },
+            headerTintColor: '#fff',
+            headerTitleStyle: { fontWeight: 'bold' },
+        }}>
+            <Stack.Screen name="index" options={{ title: 'Home' }} />
+            <Stack.Screen name="about" options={{ title: 'About' }} />
+            <Stack.Screen name="contact" options={{ title: 'Contact' }} />
+        </Stack>
     )
 }
 
