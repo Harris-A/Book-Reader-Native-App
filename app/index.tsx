57b663f374ff4ac977c39abb5e71aee4cef268d0
Harris Alam <harrisalam3@gmail.com>
2025-06-24 13:50:18 +0100

    update Home component to use themed components and themed logo

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,27 @@
-import { StyleSheet, Text, View, Image} from "react-native";
+import {StyleSheet, Text,} from "react-native";
 import {Link} from "expo-router";
 // @ts-ignore
-import Logo from '../assets/app-logo.png'
+import Logo from '../assets/app-logo-light.png'
+
+// themed components
+import ThemedView from "../components/ThemedView";
+import ThemedLogo from "../components/ThemedLogo";
 
 const Home = () => {
     return (
         /* default react-native components (View & Text) */
-        <View style={styles.container}>
-            <Image source={Logo} style={styles.img} />
+        /* dynamically change the style of the view by using CSS class rule */
+        <ThemedView style={styles.container}>
+            <ThemedLogo style={styles.img} />
 
-            {/* dynamically change the style of the view by using CSS class rule */}
             <Text style={styles.title}>My First React-Native</Text>
 
-            {/*inline css*/}
+            {/* inline css */}
             <Text style={{marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
 
             <Link href="/about" style={styles.link}>About Page</Link>
             <Link href="/contact" style={styles.link}>Contact Page</Link>
-        </View>
+        </ThemedView>
     )
 }
 
