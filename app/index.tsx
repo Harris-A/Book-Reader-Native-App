ea284c70e6b2a340746778551403ad870f2dfc6f
Harris Alam <harrisalam3@gmail.com>
2025-06-24 09:58:36 +0100

    add link navigation to view on homepage

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,22 @@
 import { StyleSheet, Text, View, Image} from "react-native";
+import {Link} from "expo-router";
+// @ts-ignore
 import Logo from '../assets/app-logo.png'
 
 const Home = () => {
     return (
-        /* default react native components (View & Text) */
+        /* default react-native components (View & Text) */
         <View style={styles.container}>
             <Image source={Logo} style={styles.img} />
 
-            {/* dynamically change the style of the view by using css class rule */}
+            {/* dynamically change the style of the view by using CSS class rule */}
             <Text style={styles.title}>My First React-Native</Text>
 
             {/*inline css*/}
             <Text style={{marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
+
+            <Link href="/about" style={styles.link}>About Page</Link>
+            <Link href="/contact" style={styles.link}>Contact Page</Link>
         </View>
     )
 }
@@ -30,7 +35,11 @@ const styles = StyleSheet.create({
     },
     img: {
         marginVertical: 20,
-        width: 100,
-        height: 100,
+        width: 150,
+        height: 150,
+    },
+    link: {
+        marginVertical: 10,
+        borderBottomWidth: 1
     }
 })
