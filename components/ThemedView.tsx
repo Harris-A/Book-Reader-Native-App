9f91e96966b9febcd8f4b360b6b62a89cece3be2
Harris Alam <harrisalam3@gmail.com>
2025-06-26 15:55:42 +0100

    update ThemedView: add SafeAreaView support with insets padding

--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -1,21 +1,40 @@
 // -- custom themed view component to override the default native VIEW component --
 
-import { View, useColorScheme } from 'react-native';
+import { SafeAreaView, View, useColorScheme } from 'react-native';
 import { COLORS } from '../constants/colours';
+import {useSafeAreaInsets} from "react-native-safe-area-context";
 
 // @ts-ignore
-const ThemedView = ({ style, ...props }) => {
+const ThemedView = ({ style, safe = false, ...props }) => {
     const colorScheme = useColorScheme() // grab a color scheme using ColorScheme hook
     // @ts-ignore
 
     // Selects the appropriate color theme (light or dark) or null. If null, then the theme will be light.
     const theme = COLORS[colorScheme] ?? COLORS.light
 
-    return (
-      <View style={[{backgroundColor: theme.background}, style]}
-      {...props}
-      />
-  )
+    // if safeView is false, return the default react-native View.
+    if (!safe)
+        return (
+            <View style={[{backgroundColor: theme.background}, style]}
+                  {...props}
+            />
+        )
+
+        const insets = useSafeAreaInsets()
+
+    //Else, return a View with safe area insets applied (top and bottom padding)
+        return (
+            <View
+                style={[{
+                    backgroundColor: theme.background,
+                    paddingTop: insets.top,
+                    paddingBottom: insets.bottom,
+                },
+                    style
+                ]}
+                {...props}
+            />
+        )
 }
 
 export default ThemedView;
