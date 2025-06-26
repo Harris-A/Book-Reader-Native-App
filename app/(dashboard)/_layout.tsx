d7f2ada3dce9936e6c6ce3a64736b6d9792998fb
Harris Alam <harrisalam3@gmail.com>
2025-06-26 15:36:37 +0100

    install expo icons for tab navigation

---
+++ app/(dashboard)/_layout.tsx
@@ -0,0 +1,59 @@
+import {Tabs} from "expo-router";
+import {useColorScheme} from "react-native";
+import {COLORS} from "../../constants/colours";
+import { Ionicons } from '@expo/vector-icons';
+
+const DashboardLayout = () => {
+    const colorScheme = useColorScheme()
+    // @ts-ignore
+    const theme = COLORS[colorScheme] ?? COLORS.light
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarStyle: {
+                    backgroundColor: theme.navBackground,
+                    paddingTop: 10,
+                    height: 100,
+                },
+                tabBarActiveTintColor: theme.iconColorFocused,
+                tabBarInactiveTintColor: theme.iconColor
+        }}
+        >
+            <Tabs.Screen
+                name="profile"
+                options={{ title: 'Profile', tabBarIcon: ({focused}) => (
+                    <Ionicons
+                        name={focused ? 'person' : 'person-outline'}
+                        size={24}
+                        color={focused ? theme.iconColorFocused: theme.iconColor}
+                    />
+                )
+            }}
+            />
+            <Tabs.Screen
+                name="books"
+                options={{ title: 'Books', tabBarIcon: ({focused}) => (
+                    <Ionicons
+                        name={focused ? 'book' : 'book-outline'}
+                        size={24}
+                        color={focused ? theme.iconColorFocused: theme.iconColor}
+                    />
+                    )
+            }}
+            />
+            <Tabs.Screen
+                name="create"
+                options={{ title: 'Create', tabBarIcon: ({focused}) => (
+                    <Ionicons
+                        name={focused ? 'create' : 'create-outline'}
+                        size={24}
+                        color={focused ? theme.iconColorFocused: theme.iconColor}
+                    />
+                    )
+            }}
+            />
+        </Tabs>
+    )
+}
+export default DashboardLayout;
