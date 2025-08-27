0c633789c0bb9e516f8b767d49eba82091716545
Harris Alam <harrisalam3@gmail.com>
2025-08-27 15:27:54 +0100

    error handling for reg form

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,20 +1,31 @@
 import { StyleSheet, Text} from "react-native";
+import { Link } from "expo-router";
+import {useState} from "react";
+import {useUser} from "../../hooks/userUser";
 
 // themed components
 import ThemedView from "../../components/ThemedView";
 import ThemedText from "../../components/ThemedText";
-import { Link } from "expo-router";
 import ThemedButton from "../../components/ThemedButton";
 import Spacer from "../../components/spacer";
 import ThemedTextInput from "../../components/ThemedTextInput";
-import {useState} from "react";
 
-const register = () => {
+const Register = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
 
-    const handleSubmit = () => {
-        console.log('register form submitted', email, password)
+    const { user, register } = useUser()
+
+    const handleSubmit = async () => {
+
+        setError('')
+        try {
+            await register(email, password)
+        } catch (error) {
+            // @ts-ignore
+            setError(error.message)
+        }
     }
 
     return (
@@ -45,6 +56,9 @@ const register = () => {
             </ThemedButton>
 
             <Spacer />
+            {error && <ThemedText style={styles.error}>{error}</ThemedText>}
+
+            <Spacer />
 
             <Link href='/login'>
                 <ThemedText style={{fontSize: 12}}>
@@ -56,7 +70,7 @@ const register = () => {
     )
 }
 
-export default register
+export default Register
 
 const styles = StyleSheet.create({
     container: {
@@ -69,4 +83,13 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         textAlign: 'center',
     },
+    error: {
+        color: 'COLORS.warning',
+        padding: 10,
+        backgroundColor: 'COLORS.errorBackground',
+        borderColor: 'COLORS.errorBorder',
+        borderWidth: 1,
+        borderRadius: 5,
+        marginHorizontal: 10,
+    }
 })
